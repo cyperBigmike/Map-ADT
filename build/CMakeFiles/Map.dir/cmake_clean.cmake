c3da0507c7a43b524172e8f5f6cd192a4f62f0ed
Mohammad Mossa <mohammed.19.03@hotmail.com>
2020-04-14 16:42:10 +0300

    Map initialilization

---
+++ build/CMakeFiles/Map.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Map.dir/map.c.obj"
+  "Map.exe"
+  "Map.exe.manifest"
+  "Map.pdb"
+  "libMap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Map.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
